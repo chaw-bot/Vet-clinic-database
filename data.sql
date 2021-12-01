ff8f1c9042ea1709d70a05959ec550b6d402280a
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-01 23:49:20 +0200

    Insert species_id and owner_id to the animals

--- data.sql
+++ data.sql
@@ -1,4 +1,67 @@
 /* Populate database with sample data. */
+BEGIN;
+
+-- insert data into owners table
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Sam Smith',
+  34
+);
+
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Jennifer Orwell',
+  19
+);
+
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Bob',
+  45
+);
+
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Melody Pond',
+  77
+);
+
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Dean Winchester',
+  14
+);
+
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Jodie Whittaker',
+  38
+);
+
+-- insert data into species table
+INSERT INTO species (
+  name
+) VALUES (
+  'Pokemon'
+);
+
+INSERT INTO species (
+  name
+) VALUES (
+  'Digimon'
+);
 
 -- Agumon's Profile
 INSERT INTO animals (
@@ -6,13 +69,17 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
-  weight_kg
+  weight_kg,
+  species_id,
+  owner_id
 ) VALUES (
   'Agumon',
   '2020-2-3',
   0,
   true,
-  10.23
+  10.23,
+  2,
+  1
 );
 
 -- Gabumon's Profile
@@ -21,13 +88,17 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
-  weight_kg
+  weight_kg,
+  species_id,
+  owner_id
 ) VALUES (
   'Gabumon',
   '2018-11-15',
   2,
   true,
-  8
+  8,
+  2,
+  2
 );
 
 -- Pikachu's Profile
@@ -36,13 +107,17 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
-  weight_kg
+  weight_kg,
+  species_id,
+  owner_id
 ) VALUES (
   'Pikachu',
   '2021-1-7',
   1,
   false,
-  15.04
+  15.04,
+  1,
+  2
 );
 
 -- Devimon's Profile
@@ -51,13 +126,17 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
-  weight_kg
+  weight_kg,
+  species_id,
+  owner_id
 ) VALUES (
   'Devimon',
   '2017-5-12',
   5,
   true,
-  11
+  11,
+  2,
+  3
 );
 
 -- Charmander's Profile
@@ -66,13 +145,17 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
-  weight_kg
+  weight_kg,
+  species_id,
+  owner_id
 ) VALUES (
   'Charmander',
   '2020-2-8',
   0,
   false,
-  11
+  11,
+  1,
+  4
 );
 
 -- Plantmon's Profile
@@ -81,13 +164,17 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
-  weight_kg
+  weight_kg,
+  species_id,
+  owner_id
 ) VALUES (
   'Plantmon',
   '2022-11-15',
   2,
   true,
-  5.7
+  5.7,
+  2,
+  3
 );
 
 -- Squirtle's Profile
@@ -96,13 +183,17 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
-  weight_kg
+  weight_kg,
+  species_id,
+  owner_id
 ) VALUES (
   'Squirtle',
   '1993-4-2',
   3,
   false,
-  12.13
+  12.13,
+  1,
+  4
 );
 
 -- Angemon's Profile
@@ -111,13 +202,17 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
-  weight_kg
+  weight_kg,
+  species_id,
+  owner_id
 ) VALUES (
   'Angemon',
   '2005-6-12',
   1,
   true,
-  45
+  45,
+  2,
+  5
 );
 
 -- Boarmon's Profile
@@ -126,13 +221,17 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
-  weight_kg
+  weight_kg,
+  species_id,
+  owner_id
 ) VALUES (
   'Boarmon',
   '2005-6-7',
   7,
   true,
-  20.4
+  20.4,
+  2,
+  5
 );
 
 -- Blossom's Profile
@@ -141,11 +240,17 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
-  weight_kg
+  weight_kg,
+  species_id,
+  owner_id
 ) VALUES (
   'Blossom',
   '1998-10-13',
   3,
   true,
-  17
+  17,
+  1,
+  4
 );
+
+COMMIT;
